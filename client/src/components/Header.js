255c9e5e2be3f588471c0448843ac4d9d6f7edc0
nathanbaleeta <nbaleeta@gmail.com>
2019-02-26 13:16:13 +0300

    Switched Header to class component and added search box

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import GroupIcon from "@material-ui/icons/Group";
-import MessageIcon from "@material-ui/icons/Message";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import { withStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
-const styles = {
+const styles = theme => ({
   root: {
-    flexGrow: 1
+    width: "100%"
   },
   grow: {
     flexGrow: 1
@@ -27,59 +37,240 @@ const styles = {
   link: {
     textDecoration: "none",
     color: "white"
+  },
+  avatar: {
+    margin: 10
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "white",
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.75)
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    color: "black",
+    "&:hover": {
+      color: fade(theme.palette.common.white, 0.25)
+    },
+    width: theme.spacing.unit * 9,
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "primary",
+    width: "100%"
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
   }
-};
+});
 
-function Header(props) {
-  const { classes } = props;
+class Header extends React.Component {
+  state = {
+    anchorEl: null,
+    mobileMoreAnchorEl: null
+  };
 
-  return (
-    <div className={classes.root}>
-      <AppBar
-        position="static"
-        color="primary"
-        style={{ background: "#D23E56" }}
+  handleProfileMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+    this.handleMobileMenuClose();
+  };
+
+  handleMobileMenuOpen = event => {
+    this.setState({ mobileMoreAnchorEl: event.currentTarget });
+  };
+
+  handleMobileMenuClose = () => {
+    this.setState({ mobileMoreAnchorEl: null });
+  };
+
+  render() {
+    const { anchorEl, mobileMoreAnchorEl } = this.state;
+    const { classes } = this.props;
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const renderMenu = (
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMenuOpen}
+        onClose={this.handleMenuClose}
       >
-        <Toolbar>
-          <Typography variant="h5" color="inherit" className={classes.grow}>
-            b1gplay
-          </Typography>
-          <div>
-            <Link to="/profile" className={classes.link}>
-              <Button color="inherit" className={classes.menuButton}>
-                Profile
-              </Button>
-            </Link>
-            <Link to="/" className={classes.link}>
-              <Button color="inherit" className={classes.menuButton}>
-                Home
-              </Button>
-            </Link>
-            <Link to="/find-players" className={classes.link}>
-              <Button color="inherit" className={classes.menuButton}>
-                Find Players
-              </Button>
-            </Link>
-            <Link to="/talent-suggestions" className={classes.link}>
-              <IconButton color="inherit">
-                <GroupIcon />
+        <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={this.handleMenuClose}>Settings</MenuItem>
+        <MenuItem onClick={this.handleMenuClose}>Logout</MenuItem>
+      </Menu>
+    );
+
+    const renderMobileMenu = (
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMobileMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <MenuItem>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+          <p>Messages</p>
+        </MenuItem>
+
+        <MenuItem onClick={this.handleMobileMenuClose}>
+          <IconButton color="inherit">
+            <Badge badgeContent={11} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <p>Notifications</p>
+        </MenuItem>
+        <MenuItem onClick={this.handleProfileMenuOpen}>
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+          <p>Profile</p>
+        </MenuItem>
+      </Menu>
+    );
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" style={{ background: "#D23E56" }}>
+          <Toolbar>
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              b1gplay
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
+            </div>
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+              <div>
+                <Grid container justify="center" alignItems="center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                    className={classes.avatar}
+                  />
+                  <Link to="/profile" className={classes.link}>
+                    <Button color="inherit" className={classes.menuButton}>
+                      Profile
+                    </Button>
+                  </Link>
+                  <Link to="/" className={classes.link}>
+                    <Button color="inherit" className={classes.menuButton}>
+                      Home
+                    </Button>
+                  </Link>
+                  <Link to="/find-players" className={classes.link}>
+                    <Button color="inherit" className={classes.menuButton}>
+                      Find Players
+                    </Button>
+                  </Link>
+                  <Link to="/messages" className={classes.link}>
+                    <IconButton color="inherit">
+                      <Badge badgeContent={4} color="primary">
+                        <MailIcon />
+                      </Badge>
+                    </IconButton>
+                  </Link>
+                  <Link to="/notifications" className={classes.link}>
+                    <IconButton color="inherit">
+                      <Badge badgeContent={17} color="primary">
+                        <NotificationsIcon />
+                      </Badge>
+                    </IconButton>
+                  </Link>
+                </Grid>
+              </div>
+
+              <IconButton
+                aria-owns={isMenuOpen ? "material-appbar" : undefined}
+                aria-haspopup="true"
+                onClick={this.handleProfileMenuOpen}
+                color="inherit"
+              >
+                <ArrowDropDownIcon />
               </IconButton>
-            </Link>
-            <Link to="/messages" className={classes.link}>
-              <IconButton color="inherit">
-                <MessageIcon />
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton
+                aria-haspopup="true"
+                onClick={this.handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
               </IconButton>
-            </Link>
-            <Link to="/notifications" className={classes.link}>
-              <IconButton color="inherit">
-                <NotificationsIcon />
-              </IconButton>
-            </Link>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+            </div>
+          </Toolbar>
+        </AppBar>
+        {renderMenu}
+        {renderMobileMenu}
+      </div>
+    );
+  }
 }
 
 Header.propTypes = {
