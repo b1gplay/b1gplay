4fd1abeaeab5ceb3fc6375280cbd0e22692d94f2
nathanbaleeta <nbaleeta@gmail.com>
2019-07-08 13:13:44 +0300

    Fixed SportsForm:accountType field to persist to redux external state

--- client/src/components/Authentication/SportsForm.js
+++ client/src/components/Authentication/SportsForm.js
@@ -4,7 +4,6 @@ import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
-import MenuList from "@material-ui/core/MenuList";
 
 import { connect } from "react-redux";
 import { updateField } from "../../actions/wizard";
@@ -14,259 +13,250 @@ const styles = theme => ({
     margin: theme.spacing.unit,
     minWidth: "100%",
     maxWidth: "auto"
+  },
+  selectField: {
+    margin: theme.spacing.unit * 3,
+    width: "100%",
+    textAlign: "left",
+    fontFamily: "Arial",
+    color: "black",
+    marginLeft: 0,
+    lineHeight: "40px"
   }
 });
+
+const accounts = [
+  {
+    value: "Coach",
+    label: "Coach"
+  },
+  {
+    value: "Player",
+    label: "Player"
+  },
+  {
+    value: "Media",
+    label: "Media"
+  },
+  {
+    value: "Fan",
+    label: "Fan"
+  },
+  {
+    value: "Agent",
+    label: "Agent"
+  }
+];
 
 class SportsForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      playerFormHidden: true,
-      mediaFormHidden: true,
-      commonFormHidden: true
-    };
+    this.state = { accountForm: "" };
   }
 
   onChange = e => {
     this.props.updateField(e.target.name, e.target.value);
   };
 
-  showPlayerFormHidden() {
-    this.setState({
-      playerFormHidden: false,
-      mediaFormHidden: true,
-      commonFormHidden: true
-    });
-  }
-
-  hidePlayerFormHidden() {
-    this.setState({
-      playerFormHidden: true
-    });
-  }
-
-  hideCommonFormHidden() {
-    this.setState({
-      commonFormHidden: true
-    });
-  }
-
-  showCommonFormHidden() {
-    this.setState({
-      commonFormHidden: false,
-      mediaFormHidden: true,
-      playerFormHidden: true
-    });
-  }
-
-  hideMediaFormHidden() {
-    this.setState({
-      mediaFormHidden: false
-    });
-  }
-
-  showMediaFormHidden() {
-    this.setState({
-      mediaFormHidden: false,
-      playerFormHidden: true,
-      commonFormHidden: true
-    });
-  }
-
   render() {
     const { classes } = this.props;
 
+    const common = (
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={12}>
+          <TextField
+            id="outlined-uncontrolled"
+            label="Present club/ Affiliation"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="affiliation"
+            value={this.props.wizard.affiliation}
+            onChange={this.onChange}
+          />
+        </Grid>
+      </Grid>
+    );
+
+    const media = (
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={12}>
+          <TextField
+            id="outlined-uncontrolled"
+            label="Media House"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="mediaHouse"
+            value={this.props.wizard.mediaHouse}
+            onChange={this.onChange}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Grid>
+      </Grid>
+    );
+
+    const player = (
+      <Grid container spacing={8}>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="outlined-uncontrolled"
+            label="Height"
+            type="number"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="height"
+            value={this.props.wizard.height}
+            onChange={this.onChange}
+          />
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="outlined-uncontrolled"
+            label="Position"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="position"
+            value={this.props.wizard.position}
+            onChange={this.onChange}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="outlined-uncontrolled"
+            label="Wingspan"
+            type="number"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="wingspan"
+            value={this.props.wizard.wingspan}
+            onChange={this.onChange}
+          />
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="outlined-uncontrolled"
+            label="Vertical leap"
+            type="number"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="verticalLeap"
+            value={this.props.wizard.verticalLeap}
+            onChange={this.onChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="outlined-uncontrolled"
+            label="Time to run 40m"
+            type="number"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="time40m"
+            value={this.props.wizard.time40m}
+            onChange={this.onChange}
+          />
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="outlined-uncontrolled"
+            label="Time to run 100m"
+            type="number"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="time100m"
+            value={this.props.wizard.time100m}
+            onChange={this.onChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <TextField
+            id="outlined-uncontrolled"
+            label="Present club/ Affiliation"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            name="affiliation"
+            value={this.props.wizard.affiliation}
+            onChange={this.onChange}
+          />
+        </Grid>
+      </Grid>
+    );
+
     return (
       <div>
-        <Grid container spacing={8}>
-          <Grid item xs={12} sm={12}>
-            <TextField
-              required
-              id="accountType"
-              select
-              label="Account type"
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              helperText="Please select account type"
-              InputLabelProps={{
-                shrink: true
-              }}
-              name="accountType"
-              value={this.props.wizard.accountType}
-              onChange={this.onChange}
-            >
-              <MenuList>
-                <MenuItem onClick={this.showCommonFormHidden.bind(this)}>
-                  Coach
-                </MenuItem>
-
-                <MenuItem onClick={this.showPlayerFormHidden.bind(this)}>
-                  Player
-                </MenuItem>
-
-                <MenuItem onClick={this.showMediaFormHidden.bind(this)}>
-                  Media
-                </MenuItem>
-
-                <MenuItem onClick={this.showCommonFormHidden.bind(this)}>
-                  Fan
-                </MenuItem>
-
-                <MenuItem onClick={this.showCommonFormHidden.bind(this)}>
-                  Agent
-                </MenuItem>
-              </MenuList>
-            </TextField>
-          </Grid>
-          {!this.state.playerFormHidden && (
-            <div className="modal">
-              <Grid container spacing={8}>
-                <Grid item xs={6} sm={6}>
-                  <TextField
-                    required
-                    id="outlined-uncontrolled"
-                    label="Position"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                    fullWidth
-                    name="position"
-                    value={this.props.wizard.position}
-                    onChange={this.onChange}
-                  />
-                </Grid>
-                <Grid item xs={6} sm={6}>
-                  <TextField
-                    required
-                    id="outlined-uncontrolled"
-                    label="Height"
-                    type="number"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                    fullWidth
-                    name="height"
-                    value={this.props.wizard.height}
-                    onChange={this.onChange}
-                  />
-                </Grid>
-
-                <Grid item xs={6} sm={6}>
-                  <TextField
-                    required
-                    id="outlined-uncontrolled"
-                    label="Wingspan"
-                    type="number"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                    fullWidth
-                    name="wingspan"
-                    value={this.props.wizard.wingspan}
-                    onChange={this.onChange}
-                  />
-                </Grid>
-                <Grid item xs={6} sm={6}>
-                  <TextField
-                    required
-                    id="outlined-uncontrolled"
-                    label="Vertical leap"
-                    type="number"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                    fullWidth
-                    name="verticalLeap"
-                    value={this.props.wizard.verticalLeap}
-                    onChange={this.onChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="outlined-uncontrolled"
-                    label="Time to run 40m"
-                    type="number"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                    fullWidth
-                    name="time40m"
-                    value={this.props.wizard.time40m}
-                    onChange={this.onChange}
-                  />
-                </Grid>
-                <Grid item xs={6} sm={6}>
-                  <TextField
-                    required
-                    id="outlined-uncontrolled"
-                    label="Time to run 100m"
-                    type="number"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                    fullWidth
-                    name="time100m"
-                    value={this.props.wizard.time100m}
-                    onChange={this.onChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={12}>
-                  <TextField
-                    id="outlined-uncontrolled"
-                    label="Present club/ Affiliation"
-                    className={classes.textField}
-                    margin="normal"
-                    variant="outlined"
-                    fullWidth
-                    name="presentClub"
-                    value={this.props.wizard.presentClub}
-                    onChange={this.onChange}
-                  />
-                </Grid>
-              </Grid>
-            </div>
-          )}
+        <Grid item xs={12} sm={12}>
+          <TextField
+            required
+            id="accountType"
+            select
+            name="accountType"
+            value={this.props.wizard.accountType}
+            onChange={this.onChange}
+            label="Account type"
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            className={classes.selectField}
+            helperText="Please select title"
+            InputLabelProps={{
+              shrink: true
+            }}
+          >
+            {accounts.map(option => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
         </Grid>
-        {!this.state.mediaFormHidden && (
-          <div className="modal">
-            <Grid container spacing={0}>
-              <Grid item xs={12} sm={12}>
-                <TextField
-                  id="outlined-uncontrolled"
-                  label="Media House"
-                  className={classes.textField}
-                  margin="normal"
-                  variant="outlined"
-                  fullWidth
-                  name="mediaHouse"
-                  value={this.props.wizard.mediaHouse}
-                  onChange={this.onChange}
-                />
-              </Grid>
-            </Grid>
-          </div>
-        )}
-        {!this.state.commonFormHidden && (
-          <div className="modal">
-            <Grid container spacing={0}>
-              <Grid item xs={12} sm={12}>
-                <TextField
-                  id="outlined-uncontrolled"
-                  label="Present club/ Affiliation"
-                  className={classes.textField}
-                  margin="normal"
-                  variant="outlined"
-                  fullWidth
-                  name="affiliation"
-                  value={this.props.wizard.affiliation}
-                  onChange={this.onChange}
-                />
-              </Grid>
-            </Grid>
-          </div>
-        )}
+
+        {/* Using switch to select the appropriate case onChange account type*/}
+        {(() => {
+          switch (this.props.wizard.accountType) {
+            case "Player":
+              return player;
+            case "Media":
+              return media;
+            case "Coach":
+              return common;
+            case "Fan":
+              return common;
+            case "Agent":
+              return common;
+            default:
+              return "";
+          }
+        })()}
+        {/* Using switch to select the appropriate case onChange account type*/}
       </div>
     );
   }
