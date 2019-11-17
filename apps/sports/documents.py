5b98307fa378c2e8140559b736fde5ad457c672f
nathanbaleeta <nbaleeta@gmail.com>
2019-11-18 00:24:59 +0300

    Added foreign key support to elastic search indexing of metrics

--- apps/sports/documents.py
+++ apps/sports/documents.py
@@ -1,11 +1,21 @@
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
 from apps.sports.models.basketball_ratings import BasketballRatings
+from apps.users.models.profile import Profile
 
 
 @registry.register_document
 class BasketballRatingsDocument(Document):
+    ratedBy = fields.NestedField(properties={
+        'pk': fields.TextField(),
+        'account_type': fields.TextField(),
+
+    })
+    player = fields.NestedField(properties={
+        'pk': fields.TextField(),
+    })
+
     class Index:
         # Name of the Elasticsearch index
         name = 'metrics'
@@ -27,7 +37,9 @@ class BasketballRatingsDocument(Document):
             "energy",
             "determination",
             "clutch",
-            "proPotential"
+            "proPotential",
+
+
 
         ]
 
@@ -41,3 +53,13 @@ class BasketballRatingsDocument(Document):
         # Paginate the django queryset used to populate the index with the specified size
         # (by default it uses the database driver's default setting)
         # queryset_pagination = 5000ss
+
+        def get_instances_from_related(self, related_instance):
+            """If related_models is set, define how to retrieve the Car instance(s) from the related model.
+            The related_models option should be used with caution because it can lead in the index
+            to the updating of a lot of items.
+            """
+            if isinstance(related_instance, Profile):
+                return related_instance.basketballRatings_set.all()
+            elif isinstance(related_instance, Profile):
+                return related_instance.basketballRatings
